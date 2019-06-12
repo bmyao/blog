63fc94e92e35a356a22a0503c2030e7aea1a7a56
liangjiayao <515419008@qq.com>
2019-06-12 15:08:46 +0800

    初步创建项目

---
+++ blog-core/src/main/java/com/oneself/blog/blogcore/BlogCoreApplication.java
@@ -0,0 +1,13 @@
+package com.oneself.blog.blogcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCoreApplication.class, args);
+    }
+
+}
