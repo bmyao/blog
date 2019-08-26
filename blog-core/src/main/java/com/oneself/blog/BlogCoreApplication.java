27a1ec68dc2b1cadee27794e5be792f8c95c29c8
liangjiayao <515419008@qq.com>
2019-08-26 12:02:08 +0800

    1

--- blog-core/src/main/java/com/oneself/blog/BlogCoreApplication.java
+++ blog-core/src/main/java/com/oneself/blog/BlogCoreApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.oneself.blog.**.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = "com.oneself.blog.common.config")
 public class BlogCoreApplication {
 
     public static void main(String[] args) {
